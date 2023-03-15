24c29ec15092e37701b585e2bf81b12b1990e53e
cob05 <chris.obrien05@gmail.com>
2023-03-15 03:30:05 -0400

    added session vars sample

---
+++ session_vars_sample.py
@@ -0,0 +1,6 @@
+## RENAME THIS FILE TO session_vars.py AND FILL IN VALUES LIKE NORMAL ##
+## Session Variables (update every time you login or your browser updates)
+USER_ID = ""
+USER_AGENT = ""
+SESS_COOKIE = ""
+X_BC = ""
